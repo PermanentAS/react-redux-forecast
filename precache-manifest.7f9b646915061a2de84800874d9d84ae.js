58ccea6ba1305b4b92d72f39823a580466e696e0
Anton Shlykov <permanent.as@gmail.com>
2019-08-29 15:59:14 +0300

    Updates

---
+++ precache-manifest.7f9b646915061a2de84800874d9d84ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8212709feafacecddd511beca23aef",
+    "url": "/react-redux-forecast/index.html"
+  },
+  {
+    "revision": "8da34a5dcfb11a7499c0",
+    "url": "/react-redux-forecast/static/css/main.d240ba5e.chunk.css"
+  },
+  {
+    "revision": "f4cc627cb901d046bfa2",
+    "url": "/react-redux-forecast/static/js/2.90acd1a1.chunk.js"
+  },
+  {
+    "revision": "8da34a5dcfb11a7499c0",
+    "url": "/react-redux-forecast/static/js/main.09aeb9c2.chunk.js"
+  },
+  {
+    "revision": "ee9dd7ccdab753ce82bf",
+    "url": "/react-redux-forecast/static/js/runtime~main.ed024d0a.js"
+  }
+]);
