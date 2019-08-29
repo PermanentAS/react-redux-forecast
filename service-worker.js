56a71217c580e9014fdf04fd957282d10c938759
Anton Shlykov <permanent.as@gmail.com>
2019-08-29 16:46:52 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-redux-forecast/precache-manifest.1292eef5c2014ee18dacf9b779b46b7f.js"
+  "/react-redux-forecast/precache-manifest.7f9b646915061a2de84800874d9d84ae.js"
 );
 
 self.addEventListener('message', (event) => {
